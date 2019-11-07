838cd22c0bfb183fb1fb031a8f8d8faea780b433
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-11-06 20:11:17 -0600

    added moment.js calculations, finished javascript and confirmed trains track

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -3,6 +3,8 @@ const database = firebase.database();
 
 //Push to database
 $("#submitTrain").on("click", function (event) {
+    event.preventDefault();
+
     const trainName = $("#TrainName").val().trim();
     const destination = $("#destinationName").val().trim();
     const firstTrain = $("#firstTrainTime").val().trim();
@@ -14,19 +16,33 @@ $("#submitTrain").on("click", function (event) {
         firstTrain,
         freqTrain,
     });
+
+    $("#TrainName").val("");
+    $("#destinationName").val("");
+    $("#firstTrainTime").val("");
+    $("#frequencyTrain").val("");
 });
 
 //Firebase watcher adds to html after child added
 database.ref().on("child_added", function(trainCreated){
-    //log the snapshots
-    console.log(trainCreated.val().trainName);
-    console.log(trainCreated.val().destination);
-    console.log(trainCreated.val().firstTrain);
-    console.log(trainCreated.val().freqTrain);
+
+    let tFrequency = trainCreated.val().freqTrain;
+    let firstTime = trainCreated.val().firstTrain;
+    const firstTimeConverted = moment(firstTime,"HH:mm").subtract(1,"years");
+    const currentTime = moment();
+    const diffTime = moment().diff(moment(firstTimeConverted),"minutes");
+    const tRemainder = diffTime % tFrequency;
+    const tMinutesTillTrain = tFrequency - tRemainder;
+    const nextTrain = moment().add(tMinutesTillTrain,"minutes");
+    
+
 
     const newRow = $("<tr>");
     newRow.append(`<td> ${trainCreated.val().trainName}</td>`);
     newRow.append(`<td> ${trainCreated.val().destination}</td>`);
     newRow.append(`<td> ${trainCreated.val().firstTrain}</td>`);
     newRow.append(`<td> ${trainCreated.val().freqTrain}</td>`);
-})
+    newRow.append(`<td> ${moment(nextTrain).format("hh:mm")}</td>`);
+    newRow.append(`<td> ${tMinutesTillTrain}</td>`);
+    $("tbody").append(newRow);
+});
